438e51dd141455e43a9162881ffdc77e72c82919
mj0d3k <a.oleksy.m@gmail.com>
2023-04-01 15:44:43 +0200

    Dodane koło

--- exercise/ex_2*.py
+++ exercise/ex_2*.py
@@ -11,3 +11,11 @@ b=10
 obwod=2*a+2*b
 pole=a*b
 print("Obwód prostokątu wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
+
+#koło
+r = 5
+π = 3.14
+obwod = 2*π*r
+pole=π*(r*r)
+print("Obwód koła wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
+
