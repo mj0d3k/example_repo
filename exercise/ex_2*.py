6a534efd5d16b20d5fc407da12fe620b275ec109
mj0d3k <a.oleksy.m@gmail.com>
2023-04-01 15:43:51 +0200

    Dodany prostokąt

--- exercise/ex_2*.py
+++ exercise/ex_2*.py
@@ -4,3 +4,10 @@ obwod = a * 4
 pole = a * a
 
 print("Obwód kwadratu wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
+
+#prostokąt
+a=5
+b=10
+obwod=2*a+2*b
+pole=a*b
+print("Obwód prostokątu wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
