1b215a5a4884c5f48fadb462104ef1a835797af5
mj0d3k <a.oleksy.m@gmail.com>
2023-04-03 18:47:36 +0200

    Poprawka

--- exercise/ex_3.py
+++ exercise/ex_3.py
@@ -17,20 +17,17 @@ def prostokat(bok_a, bok_b):
 
 # rownoleglobok i romb dla studenta 2
 def rownoleglobok(bok_a, bok_b, wysokosc_a):
-    # TODO
     obwod=bok_a*2+bok_b*2
     pole=bok_a*wysokosc_a
     return obwod, pole
 
 def romb(bok, wysokosc):
-    # TODO
     obwod=bok*4
     pole=bok*wysokosc
     return obwod, pole
 
 # trapez i kolo dla studenta 3
 def trapez(bok_a, bok_b, bok_c, bok_d, wysokosc_a):
-    # TODO
     obwod=bok_a+bok_b+bok_c+bok_d
     pole=(bok_a+bok_b)*wysokosc_a/2
     return obwod, pole
@@ -43,9 +40,11 @@ def kolo(promien):
 
 
 # assert trojkat(10, 15, 16, 8) == (41, 40)
-
+# assert trojkat(15, 8, 12, 6) == (29, 45)
 # assert kwadrat(20) == (80, 400)
+# assert kwadrat(10) == (40, 100)
 # assert prostokat(12, 10) == (44, 120)
+# assert prostokat(10, 20) == (40, 200)
 # assert rownoleglobok(6, 5, 2) == (22, 12)
 # assert rownoleglobok(6, 8, 3) == (28, 18)
 # assert romb(10, 5) == (40, 50)
