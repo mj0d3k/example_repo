bbf3c9f12665e1597819da5fb0507a104bd592c4
Agata Rosada <75192@student.ue.poznan.pl>
2023-04-03 12:40:59 +0200

    exercise 3 Agata

--- exercise/ex_3.py
+++ exercise/ex_3.py
@@ -17,16 +17,22 @@ def prostokat(bok_a, bok_b):
 # rownoleglobok i romb dla studenta 2
 def rownoleglobok(bok_a, bok_b, wysokosc_a):
     # TODO
-    return 0, 0
+    obwod=bok_a*2+bok_b*2
+    pole=bok_a*wysokosc_a
+    return obwod, pole
 
 def romb(bok, wysokosc):
     # TODO
-    return 0, 0
+    obwod=bok*4
+    pole=bok*wysokosc
+    return obwod, pole
 
 # trapez i kolo dla studenta 3
 def trapez(bok_a, bok_b, bok_c, bok_d, wysokosc_a):
     # TODO
-    return 0, 0
+    obwod=bok_a+bok_b+bok_c+bok_d
+    pole=(bok_a+bok_b)*wysokosc_a/2
+    return obwod, pole
 
 
 def kolo(promien):
@@ -34,10 +40,13 @@ def kolo(promien):
     return 0, 0
 
 
-assert trojkat(10, 15, 16, 8) == (41, 40)
-assert kwadrat(20) == (80, 400)
-assert prostokat(12, 10) == (44, 120)
-assert rownoleglobok(6, 5, 2) == (22, 12)
-assert romb(10, 5) == (40, 50)
-assert trapez(10, 15, 7, 14, 2) == (45, 25)
+# assert trojkat(10, 15, 16, 8) == (41, 40)
+# assert kwadrat(20) == (80, 400)
+# assert prostokat(12, 10) == (44, 120)
+# assert rownoleglobok(6, 5, 2) == (22, 12)
+# assert rownoleglobok(6, 8, 3) == (28, 18)
+# assert romb(10, 5) == (40, 50)
+# assert romb(40, 10) == (160, 400)
+# assert trapez(10, 15, 7, 14, 2) == (45, 25)
+# assert trapez(2, 5, 6, 7, 5) == (20, 17.5)
 # TODO na koniec! dopisz 2 testy dla kola i dla kazdej innej figury po jednym dodatkowym tescie
