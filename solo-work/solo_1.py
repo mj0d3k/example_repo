a83e29081550b97121bf374b16cf6af7b9394466
mj0d3k <a.oleksy.m@gmail.com>
2023-04-17 21:36:12 +0200

    Cleaning up

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -1,23 +1,21 @@
 # zadanie 1.1
 
 hello = "Hello"
-student = "Ola"
+student_1 = "Ola"
 
-print("Hello Kasia")
 print("{} {}".format("Hello", "Ola"))
 
 # zadanie 1.2
 
-student = input("Wpisz swoje imie ")
+student_2 = input("Wpisz swoje imie ")
 
-print("Hello Ola")
-print("Hello", student)
+print("Hello", student_2)
 
 # zadanie 1.3
 
 studenci = ["Ania", "Kuba", "Piotr", "Jan"]
 liczba_studentow = len(studenci)
-print("Liczba studentow wynosi: " + str(liczba_studentow))
+print("Liczba studentow wynosi:" + str(liczba_studentow))
 
 # zadanie 1.4
 
@@ -42,14 +40,14 @@ print("Wynik wynosi:", str(wynik))
 ciag_znakow = "edbw(hdakqas(skqskahb))adwndwb(wgwidn()dsqwhjdw)"
 liczba_nawiasow_otwierajacych = ciag_znakow.count('(')
 
-print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacych))
+print("Liczba nawiasow otwierajacych wynosi:" + str(liczba_nawiasow_otwierajacych))
 
 # zadanie 1.7
 
 studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
 imiona = sorted(studenci)
 
-print("Alfabetyczna lista studentow wynosi: ")
+print("Alfabetyczna lista studentow wynosi:")
 for student in imiona:
     print(student)
 
@@ -58,7 +56,7 @@ for student in imiona:
 studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
 nazwiska = sorted(studenci, key=lambda x: x.split()[-1])
 
-print("Alfabetyczna lista studentow wynosi: ")
+print("Alfabetyczna lista studentow wynosi:")
 for student in nazwiska:
     print(student)
 
@@ -70,13 +68,14 @@ liczba_n = 0
 for student in studenci:
     if student.split()[-1].startswith("N"):
         liczba_n += 1
-print("Liczba studentow na N wynosi: ", liczba_n)
+print("Liczba studentow na N wynosi:", liczba_n)
 
 # zadanie 1.10
 
 wykres_1 = [[2, 4], [4, 4], [6, 4]]
 wykres_2 = [[2, 3], [4, 4], [6, 5]]
 wykres_3 = [[2, 3], [4, 3], [5, 4]]
+
 
 def czy_funkcja_liniowa(punkty):
     x1, y1 = punkty[0]
@@ -86,6 +85,7 @@ def czy_funkcja_liniowa(punkty):
         if (x - x1) * (y2 - y1) != (y - y1) * (x2 - x1):
             return False
     return True
+
 
 wykres_1_funkcja_liniowa = czy_funkcja_liniowa(wykres_1)
 wykres_2_funkcja_liniowa = czy_funkcja_liniowa(wykres_2)
