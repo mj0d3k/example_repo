fc8ebc70d8052b889a4775c464436642456cd648
mj0d3k <a.oleksy.m@gmail.com>
2023-04-17 12:50:46 +0200

    Ania changes

--- exercise/ex_3.py
+++ exercise/ex_3.py
@@ -2,20 +2,23 @@ def trojkat(bok_a, bok_b, bok_c, wysokosc_a):
     obwod = bok_a + bok_b + bok_c
     pole = (bok_a * wysokosc_a) / 2
     return obwod, pole
+print(trojkat(10, 7, 6, 5))
+assert trojkat(3, 4, 5, 6) == (12, 9)
 
-# kwadrat, prostokat dla studenta 1
 def kwadrat(bok_a):
     obwod = 4*bok_a
     pole = bok_a**2
     return obwod, pole
-
+print(kwadrat(10))
+assert kwadrat(15) == (60, 225)
 
 def prostokat(bok_a, bok_b):
     obwod = 2*bok_a + 2*bok_b
     pole = bok_a*bok_b
     return obwod, pole
+print(prostokat(10,20))
+assert prostokat(4, 10) == (28, 40)
 
-# rownoleglobok i romb dla studenta 2
 def rownoleglobok(bok_a, bok_b, wysokosc_a):
     obwod=bok_a*2+bok_b*2
     pole=bok_a*wysokosc_a
@@ -37,7 +40,6 @@ def kolo(promien):
     obwod = promien*2*π
     pole = π*promien**2
     return obwod, pole
-
 
 # assert trojkat(10, 15, 16, 8) == (41, 40)
 # assert trojkat(15, 8, 12, 6) == (29, 45)
