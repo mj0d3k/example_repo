aae11886d7933922080808133020413dfd0fd641
mj0d3k <a.oleksy.m@gmail.com>
2023-04-17 18:33:16 +0200

    Last task and clearing

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -3,8 +3,6 @@
 hello = "Hello"
 student = "Ola"
 
-# oczekiwany rezultat: Hello Ola
-# wykorzystaj w princie zmienne hello i student
 print("Hello Kasia")
 print("{} {}".format("Hello", "Ola"))
 
@@ -49,7 +47,7 @@ print("Liczba nawiasow otwierajacych wynosi: " + str(liczba_nawiasow_otwierajacy
 # zadanie 1.7
 
 studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
-imiona = sorted(studenci, key=lambda x: x.split()[0])
+imiona = sorted(studenci)
 
 print("Alfabetyczna lista studentow wynosi: ")
 for student in imiona:
@@ -66,7 +64,6 @@ for student in nazwiska:
 
 # zadanie 1.9
 
-# policz wszystkich studentow z tablicy, ktorych nazwisko zaczyna sie na N
 studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
 
 liczba_n = 0
@@ -75,23 +72,24 @@ for student in studenci:
         liczba_n += 1
 print("Liczba studentow na N wynosi: ", liczba_n)
 
-
 # zadanie 1.10
 
-# zmienne poniezej preprezentuja ulozenie punktow na wykresie,
-# do zadania dolaczono takze rysunek pomocniczy
 wykres_1 = [[2, 4], [4, 4], [6, 4]]
 wykres_2 = [[2, 3], [4, 4], [6, 5]]
 wykres_3 = [[2, 3], [4, 3], [5, 4]]
 
-# zbadaj kazdy wykres, czy dla wyznaczonych punktow istnieje funkcja
-# liniowa laczaca punkty
-# jesli sie nie da, to zwroc False
-# jesli sie da, zwroc True
+def czy_funkcja_liniowa(punkty):
+    x1, y1 = punkty[0]
+    x2, y2 = punkty[1]
+    for i in range(2, len(punkty)):
+        x, y = punkty[i]
+        if (x - x1) * (y2 - y1) != (y - y1) * (x2 - x1):
+            return False
+    return True
 
-wykres_1_funkcja_liniowa = False
-wykres_2_funkcja_liniowa = False
-wykres_3_funkcja_liniowa = False
+wykres_1_funkcja_liniowa = czy_funkcja_liniowa(wykres_1)
+wykres_2_funkcja_liniowa = czy_funkcja_liniowa(wykres_2)
+wykres_3_funkcja_liniowa = czy_funkcja_liniowa(wykres_3)
 
 if wykres_1_funkcja_liniowa:
     print("Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.")
@@ -107,8 +105,3 @@ if wykres_3_funkcja_liniowa:
     print("Dla punktow w wykres_3 mozna wyznaczyc funkcje liniowa.")
 else:
     print("Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.")
-
-# oczekiwany rezultat:
-# Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.
-# Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.
-# Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.
