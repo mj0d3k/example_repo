0d67b392e2111179bca46b31769c3e54e96c698a
Ania Oleksy <a.oleksy.m@gmail.com>
2023-05-23 18:43:28 +0200

    Pierwsze zadania z rekurencji

---
+++ recursive_tasks/ex_1.py
@@ -0,0 +1,29 @@
+# fib
+# max of list
+# silnia
+# sudoku
+
+# sum of list
+
+"""
+pseudokod:
+jezeli lista jest pusta
+to 0
+jesli nie jest pusta
+do pierwszego elemenu dodaj sumę kolejnych elementow listy
+"""
+
+
+def zad1(lista: list[float]) -> float:
+    if len(lista) == 0:
+        return 0
+    suma_listy = 0
+    for el in lista:
+        suma_listy += el
+    return suma_listy
+
+
+print(zad1([]))
+print(zad1([1]))
+print(zad1([1, 2]))
+print(zad1([1, 2, 3]))
