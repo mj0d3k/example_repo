a1e573b04365ae0a30c9c02250aff68a09032671
Ania Oleksy <a.oleksy.m@gmail.com>
2023-05-29 12:52:23 +0200

    Zadanie do kontynuacji

--- salary/ex_1.py
+++ salary/ex_1.py
@@ -2,8 +2,13 @@
 # netto 3000
 # 600 zl po stornie pracownika
 # 600 po stronie pracodawcy
-# koszt calkowity pracodawcy 3000+600+600
-# rozbic na wszytskie skladki po stronie pracodawcy
+# rozbic na wszytskie skladki po stronie pracodawcy i pracownika
+
+import csv
+
+with open('salary.csv') as my_file:
+    my_reader = csv.reader(my_file)
+
 class Pracownik:
     def __init__(self):
         self.imie= ""
@@ -13,20 +18,36 @@ class Pracownik:
     def __str__(self):
         pass
 
-    def oblicz_netto(self):
-        return 0
+    @classmethod
+    def placa_netto(cls, file):
+        pracownicy = []
+        with open(file) as pracownicy_file:
+            for row in csv.reader(pracownicy_file):
+                pracownicy.append(
+                    cls(row[0], row[1], row[2])
+                )
+        return pracownicy
 
-    def oblicz_koszty(self):
-        return 0
+print(Pracownik.placa_netto('salary.csv'))
+
+    # def placa_netto(self):
+    #     for pracownik in pracownicy:
+    #         suma = self.wynagrodzenie_brutto # czy to jest potrzebne?
+    #         return suma - self.wynagrodzenie_brutto * 0.0976 - self.wynagrodzenie_brutto * 0.015 - self.wynagrodzenie_brutto * 0.09 - self.wynagrodzenie_brutto * 0.0245
+
+    # def oblicz_koszty_pracodawcy(self):
+    #     for pracownik in pracownicy:
+    #         suma = self.wynagrodzenie_brutto
+    #         return suma + self.wynagrodzenie_brutto * 0.0976 + self.wynagrodzenie_brutto * 0.065 + self.wynagrodzenie_brutto * 0.0245 + self.wynagrodzenie_brutto * 0.067 + self.wynagrodzenie_brutto * 0.01
 
 
-    koszty_wszystkich_pracownikow = 0
-    pracownicy  = []
-    for pracownik in pracownicy:
-        print("Pracownik Kowalski: ")
-        print("- pensja brutto: ")
-        print("- pensja netto: ")
-        print("- koszty pracodawcy: ")
-        print("- koszt całkowity: ")
 
-    print("Suma kosztów wynosi: ")
+    # koszty_wszystkich_pracownikow = 0
+    # for pracownik in pracownicy:
+    #     print("Pracownik Kowalski: ")
+    #     print("- pensja brutto: ")
+    #     print("- pensja netto: ")
+    #     print("- koszty pracodawcy: ")
+    #     print("- koszt całkowity: ")
+    #
+    # print("Suma kosztów wynosi: ")
