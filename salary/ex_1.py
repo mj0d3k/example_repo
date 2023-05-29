be22c3373562b232515f26a74f99dad1215dcff7
Ania Oleksy <a.oleksy.m@gmail.com>
2023-05-29 12:00:50 +0200

    Tresc zadania

---
+++ salary/ex_1.py
@@ -0,0 +1,32 @@
+# brutto 3600
+# netto 3000
+# 600 zl po stornie pracownika
+# 600 po stronie pracodawcy
+# koszt calkowity pracodawcy 3000+600+600
+# rozbic na wszytskie skladki po stronie pracodawcy
+class Pracownik:
+    def __init__(self):
+        self.imie= ""
+        self.nazwisko = ""
+        self.wynagrodzenie_brutto = ""
+
+    def __str__(self):
+        pass
+
+    def oblicz_netto(self):
+        return 0
+
+    def oblicz_koszty(self):
+        return 0
+
+
+    koszty_wszystkich_pracownikow = 0
+    pracownicy  = []
+    for pracownik in pracownicy:
+        print("Pracownik Kowalski: ")
+        print("- pensja brutto: ")
+        print("- pensja netto: ")
+        print("- koszty pracodawcy: ")
+        print("- koszt całkowity: ")
+
+    print("Suma kosztów wynosi: ")
