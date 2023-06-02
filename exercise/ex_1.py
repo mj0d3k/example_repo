399a852925baa9b6722686ada7339a2e4c21cb42
Ania Oleksy <a.oleksy.m@gmail.com>
2023-06-02 13:32:27 +0200

    Zadania bez sudoku

--- exercise/ex_1.py
+++ exercise/ex_1.py
@@ -1,3 +1,130 @@
+# klasy
+class Trojkat:
+    def __init__(self, a, b, c, h_a):
+        self.bok_a = a  # te a nie muszą być takie same, mogłoby być self.bok_a = a
+        self.b = b
+        self.c = c
+        self.h_a = h_a
+        # self.obwod = a + b + c # tego nie musi być, albo to albo definiujemy funkcję w klasie
+        # self.pole = a*h_a/2
+
+    def obwod(self):
+        return self.bok_a + self.b + self.c
+
+    def pole(self):
+        return (self.bok_a * self.h_a) / 2
+
+
+class Trapez:
+    def __init__(self, a, b, c, d, h):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.h = h
+        # self.obwod = a + b + c + d
+        # self.pole = (a+b)*h/2
+
+    def obwod(self):
+        return self.a + self.b + self.c + self.d
+
+    def pole(self):
+        return (self.a + self.b) * self.h / 2
+
+
+class Student:
+    def __init__(self, imie, nazwisko, nr_indeksu):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.nr_indeksu = nr_indeksu
+        self.oceny = []
+
+    def __str__(self): # funkcja ma zwrócić naszego stringa
+        # return self.imie + " " + self.nazwisko - opcja poprawna acz brzydka
+        return f"{self.imie} {self.nazwisko} {self.nr_indeksu}"
+
+    def __int__(self):
+        return 5
+
+    def dodaj_ocene(self, ocena):
+        self.oceny.append(ocena)
+
+    def zwroc_srednia(self):
+        return sum(self.oceny) / len(self.oceny)
+
+
+class Ladowarka(): #do samochodu elektrycznego
+    def __init__(self, miasto, ulica, moc, liczba_zlacz, wartosc, producent, operator, model):
+        self.a = miasto
+        self.b = ulica
+        self.c = moc
+        self.d = liczba_zlacz
+        self.e = wartosc
+        self.f = producent
+        self.g = operator
+        self.h = model
+    def __str__(self):
+        return f"{self.h} {'|'} {self.f} {'|'} {self.g} {'|'} {self.a} {'|'} {self.b}"
+
+    def cena_ladowania(self): #ile będzie kosztowała godzina ładowania
+        if self.c > 100:
+            return 60 * 1.87
+        else:
+            return 60 * 1.18
+
+    def ubezpieczenie(self): #jaką kwotą operator zobowiązany jest pokryć ubezpieczenie ładowarki
+        if self.e >= 100000:
+            return 0.10 * self.e
+        else:
+            return 0.05 * self.e
+
+    def ile_samochodow(self): #ile samochodów może jednocześnie ładować się na jednej stacji
+        if self.c > 50 and self.c <= 100:
+            return self.d * 1
+        elif self.c > 100:
+            if self.d % 2 == 0:
+                return self.d / 2 + 1
+            elif self.d % 2 != 0:
+                return self.d - 1
+        else:
+            return 1
+
+moja_ladowarka_1 = Ladowarka("Poznan", "Niepodleglosci", 150, 5, 150000, "Sigma", "PowerDot", "Ekoenergetka Axon Easy")
+moja_ladowarka_2 = Ladowarka("Warszawa", "Horyzonalna", 60, 3, 40000, "Alfen", "Greenway", "Alfen Eve Double Pro-line")
+
+print(moja_ladowarka_1)
+print(moja_ladowarka_2)
+print(moja_ladowarka_1.cena_ladowania())
+print(moja_ladowarka_2.cena_ladowania())
+print(moja_ladowarka_1.ubezpieczenie())
+print(moja_ladowarka_2.ubezpieczenie())
+print(moja_ladowarka_1.ile_samochodow())
+print(moja_ladowarka_2.ile_samochodow())
+
+
+# printy do poprzednich zadań
+
+# student_ania = Student('Ania', 'Oleksy', 121174)
+# student_ania.dodaj_ocene(3)
+# print(student_ania)
+# print(int(student_ania))
+# print(student_ania.oceny)
+# print(student_ania.zwroc_srednia())
+
+# moj_trapez = Trapez(10, 8, 6, 5, 6)
+# print(moj_trapez)
+# print(moj_trapez.h)
+# print(moj_trapez.pole())
+# print(moj_trapez.obwod())
+
+# trojkat_rownoboczny = Trojkat(10, 10, 10, 8)
+# trojkat_ania = Trojkat(10, 8, 8, 6)
+
+# print(trojkat_rownoboczny)
+# print(trojkat_rownoboczny.bok_a)
+# print(trojkat_rownoboczny.obwod())
+# print(trojkat_ania.pole())
+# print(trojkat_ania.pole) bez dwóch dodatkowych nawiasów wywołuje pole z klasy, tj. self.pole
 def hello(a: str) -> str:
     # TODO
     return "Hello"
--- recursive_tasks/ex_1.py
+++ recursive_tasks/ex_1.py
@@ -1,18 +1,11 @@
-# fib
-# max of list
-# silnia
-# sudoku
-
-# suma z listy
-
 """
+SUMA Z LISTY
 pseudokod:
 jezeli lista jest pusta
 to 0
-jesli nie jest pusta
+jesli nie jest pusta (else)
 do pierwszego elemenu dodaj sumę kolejnych elementow listy
 """
-
 
 def zad1(lista: list[float]) -> float:
     if len(lista) == 0:
@@ -28,23 +21,19 @@ print(zad1([1]))
 print(zad1([1, 2]))
 print(zad1([1, 2, 3]))
 
-# max z listy
 
 """
+MAX Z LISTY
 pseudokod:
 jeżeli lista jest pusta zwróć None
-jeżeli lista zawiera tylko jeden element
-zwróć ten element
-w przeciwnym razie
-pierwszy_element := lista[0]
-reszta_listy := lista[1:]
-max_reszty := znajdz_max(reszta_listy)
-jeżeli pierwszy_element > max_reszty
-zwróć pierwszy_element
-w przeciwnym razie
-zwróć max_reszty
+jeżeli lista zawiera tylko jeden element zwróć ten element
+else
+pierwszy_element to pierwszy index listy [0]
+reszta_listy będzie oznaczona jako = lista[1:]
+max reszty znaleźć przez funkcję z reszty_listy
+jeżeli pierwszy_element > max_reszty -> zwróć pierwszy_element
+else -> zwróć max_reszty
 """
-
 
 def zad2(lista: list[float]) -> float | None:
     if not lista:
@@ -60,7 +49,63 @@ def zad2(lista: list[float]) -> float | None:
         else:
             return max_reszta
 
+
+print(10 * '-')
 print(zad2([]))
 print(zad2([1]))
 print(zad2([1, 2]))
 print(zad2([1, 2, 3]))
+
+
+"""
+SILNIA
+pseudokod:
+jeżeli n równa się 0 zwróć 1
+else -> n * funkcja(n-1) = zwróć wynik
+"""
+
+def zad3(n):
+    if n == 0:
+        return 1
+    else:
+        wynik = n * zad3(n - 1)
+        return wynik
+
+
+print(10 * '-')
+print(zad3(1))
+print(zad3(3))
+print(zad3(10))
+
+
+"""
+CIĄG FIBONACCIEGO
+pseudokod:
+jeżeli n równa się 0 zwróć 0
+jeżeli n równa się 1 zwróć 1
+else -> funkcja(n-1) + funkcja(n-2) 
+zwróć wynik
+"""
+
+def zad4(n):
+    if n < 0:
+        return None
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return zad4(n - 1) + zad4(n - 2)
+
+
+print(10 * '-')
+print(zad4(-10))
+print(zad4(0))
+print(zad4(10))
+
+
+"""
+SUDOKU
+pseudokod:
+
+"""
